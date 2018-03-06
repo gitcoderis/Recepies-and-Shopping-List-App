da4e6a0adde29ee6ea937b43cc3133dbe0eb129f
Marius <mdelininkaitis@gmail.com>
2018-03-06 19:18:14 +0200

    updt

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,18 @@
+// MODELIO APRASYMAS ILGAS:
+//
+// export class Ingredient {
+//   public name: string;
+//   public amount: number;
+//
+//   constructor(name: string, amount: number){
+//     this.name = name;
+//     this.amount = amount;
+//   }
+// }
+//
+//
+// TRUMPESNE MODELIO APRASYMO VERSIJA:
+export class Ingredient {
+  constructor(public name: string, public amount: number){
+  }
+}
